e32061010f2b44e021aab9a018ad6ad43d2cb14d
Sonu N Mahto <sonu.mahto362000@gmail.com>
2026-03-17 23:47:34 +0530

    port correction

--- app/api/send-emails/route.js
+++ app/api/send-emails/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
       user: senderEmail,
       pass: appPassword,
